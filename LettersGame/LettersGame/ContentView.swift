3861adfa5784f1dc34937c9f55f75ad55a0a4c66
FilipeTeodoro <fstufop@gmail.com>
2024-07-21 16:42:42 -0300

    create project

---
+++ LettersGame/LettersGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LettersGame
+//
+//  Created by FilipeSTeodoro on 21/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LettersGame/LettersGame/LettersGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  LettersGameApp.swift
+//  LettersGame
+//
+//  Created by FilipeSTeodoro on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LettersGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
